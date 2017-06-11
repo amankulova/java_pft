51acf4012a451e97281e50f5bd4638b1dcb29fd3
amankulova <aman.gulzhan@mail.ru>
2017-06-11 18:35:32 +0600

    Создание метода в классе Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,12 +7,7 @@ public class MyFirstProgram {
    hello("user");
    hello("Gulzhan");
 
- Point p = new Point();
-   p.x1 = 3;
-   p.y1 = 2;
-   p.x2 = 3;
-   p.y2 = 4;
-
+ Point p = new Point(3, 2 , 3, 4);
  System.out.println("Расстояние между точками A ( x1=" + p.x1 + "; y1=" + p.y1 + ") и Б (x2=" + p.x2 + "; y2=" + p.y2 + ") = " + distance(p));
  }
   /*
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -9,4 +9,11 @@ public class Point {
   public double y1;
   public double x2;
   public double y2;
+
+  public Point (double x1, double x2, double y1, double y2) {
+    this.x1=x1;
+    this.x2=x2;
+    this.y1=y1;
+    this.y2=y2;
+  }
 }
