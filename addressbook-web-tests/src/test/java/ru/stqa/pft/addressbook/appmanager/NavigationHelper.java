83eb04d07d1a3dc35829253464de4cb2fa235ea1
amankulova <aman.gulzhan@mail.ru>
2017-06-19 17:59:31 +0600

    шаг 3.1. - NavigationHelper, GroupHelper и ContactHelper

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by User on 19.06.2017.
+ */
+public class NavigationHelper {
+  private FirefoxDriver wd;
+
+  public NavigationHelper(FirefoxDriver wd) {
+    this.wd = wd;
+  }
+
+  public void gotoGroupPage() { wd.findElement(By.linkText("groups")).click();
+  }
+}
