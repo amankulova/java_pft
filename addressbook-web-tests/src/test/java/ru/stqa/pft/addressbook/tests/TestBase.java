0448e3112fc3a5039d6f4699aa3354d65eaa7696
amankulova <aman.gulzhan@mail.ru>
2017-06-19 17:23:29 +0600

    ДЗ 5 - шаг 2, "Заменяем наследование на делегирование"

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -0,0 +1,24 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+
+/**
+ * Created by User on 19.06.2017.
+ */
+public class TestBase {
+
+  protected final ApplicationManager app = new ApplicationManager();
+
+  @BeforeMethod
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+  @AfterMethod
+  public void tearDown() {
+    app.stop();
+  }
+
+}
