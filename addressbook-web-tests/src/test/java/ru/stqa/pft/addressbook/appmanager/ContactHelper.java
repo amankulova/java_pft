1267b9a3d4e0aa7d9e05151e9607c2cbfe3ef9f3
amankulova <aman.gulzhan@mail.ru>
2017-07-11 12:06:21 +0600

    ошибка

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -9,6 +9,8 @@ import ru.stqa.pft.addressbook.model.Contacts;
 
 import java.util.List;
 
+import static javax.swing.text.html.CSS.getAttribute;
+
 /**
  * Created by User on 22.06.2017.
  */
@@ -42,9 +44,6 @@ public class ContactHelper extends HelperBase {
   }
 
 
-  public void initContactModification(int id) {
-    wd.findElement(By.cssSelector(String.format("a[href='edit.php?id=%s']", id))).click();
-  }
 
   public void submitContactModification() {
     click(By.name("update"));
@@ -73,7 +72,7 @@ public class ContactHelper extends HelperBase {
   }
 
   public void modify(ContactData contact) {
-    initContactModification(contact.getId());
+    initContactModificationById(contact.getId());
     fillContactForm(contact);
     submitContactModification();
     contactCache = null;
@@ -110,10 +109,28 @@ public class ContactHelper extends HelperBase {
       int id = Integer.parseInt(cells.get(0).findElement(By.tagName("input")).getAttribute("value"));
       String lastname = cells.get(1).getText();
       String firstname = cells.get(2).getText();
+      String[] phones = cells.get(5).getText().split("\n");
 
-      contactCache.add(new ContactData().withId(id).withFirstname(firstname).withLastname(lastname));
+      contactCache.add(new ContactData().withId(id).withFirstname(firstname).withLastname(lastname)
+              .withHomePhone(phones[0]).withMobilePhone(phones[1]).withWorkPhone(phones[2]));
     }
     return new Contacts(contactCache);
+  }
+
+  public ContactData infoFromEditForm(ContactData contact) {
+    initContactModificationById(contact.getId());
+    String firstname = wd.findElement(By.name("firstname"), getAttribute("value"));
+    String lastname = wd.findElement(By.name("lastname"), getAttribute("value"));
+    String home = wd.findElement(By.name("home"), getAttribute("value"));
+    String mobile = wd.findElement(By.name("mobile"), getAttribute("value"));
+    String work = wd.findElement(By.name("work"), getAttribute("value"));
+    wd.navigate().back();
+    return new ContactData().withId(contact.getId()).withFirstname(firstname).withLastname(lastname)
+            .withHomePhone(home).withMobilePhone(mobile).withWorkPhone(work);
+
+  }
+  public void initContactModificationById(int id) {
+    wd.findElement(By.cssSelector(String.format("a[href='edit.php?id=%s']", id))).click();
   }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -3,29 +3,10 @@ package ru.stqa.pft.addressbook.model;
 public class ContactData {
   private int id = Integer.MAX_VALUE;
   private String firstname;
-
-
   private String lastname;
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-
-    ContactData that = (ContactData) o;
-
-    if (id != that.id) return false;
-    if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
-    return lastname != null ? lastname.equals(that.lastname) : that.lastname == null;
-  }
-
-  @Override
-  public int hashCode() {
-    int result = id;
-    result = 31 * result + (firstname != null ? firstname.hashCode() : 0);
-    result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
-    return result;
-  }
+  private String homePhone;
+  private String mobilePhone;
+  private String workPhone;
 
   public ContactData withId(int id) {
     this.id = id;
@@ -43,6 +24,40 @@ public class ContactData {
     return this;
 
   }
+
+
+
+
+
+
+
+  public String getWorkPhone() {
+    return workPhone;
+  }
+
+  public ContactData withWorkPhone(String workPhone) {
+    this.workPhone = workPhone;
+    return this;
+  }
+
+  public String getMobilePhone() {
+    return mobilePhone;
+  }
+
+  public ContactData withMobilePhone(String mobilePhone) {
+    this.mobilePhone = mobilePhone;
+    return this;
+  }
+
+  public String getHomePhone() {
+    return homePhone;
+  }
+
+  public ContactData withHomePhone(String homePhone) {
+    this.homePhone = homePhone;
+    return this;
+  }
+
 
   public int getId() {
     return id;
@@ -67,3 +82,5 @@ public class ContactData {
   }
 
 }
+
+
