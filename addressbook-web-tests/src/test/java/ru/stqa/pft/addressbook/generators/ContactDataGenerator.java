beb8850f269e6b10a43ab85b99855d85e6be9e87
amankulova <aman.gulzhan@mail.ru>
2017-07-14 10:32:18 +0600

    jCommander Для групп и контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.stqa.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -15,12 +18,27 @@ import java.util.List;
  */
 public class ContactDataGenerator {
 
-  public static void main(String[] args) throws IOException {
-    int count = Integer.parseInt(args[0]);
-    File file = new File(args[1]);
+  @Parameter(names = "-c", description = "Contact count")
+  public int count;
 
+  @Parameter(names = "-f", description = "Target file")
+  public String file;
+
+  public static void main(String[] args) throws IOException {
+    ContactDataGenerator generator = new ContactDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    } catch (ParameterException ex) {
+      jCommander.usage();
+      return;
+    }
+    generator.run();
+  }
+
+  private void run() throws IOException {
     List<ContactData> contacts = generateContacts(count);
-    save(contacts, file);
+    save(contacts,  new File (file));
 
   }
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.stqa.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.File;
@@ -13,16 +16,32 @@ import java.util.List;
  * Created by User on 13.07.2017.
  */
 public class GroupDataGenerator {
+
+  @Parameter(names = "-c", description = "Group count")
+  public int count;
+
+  @Parameter(names = "-f", description = "Target file")
+  public String file;
+
   public static void main(String[] args) throws IOException {
-    int count = Integer.parseInt(args[0]);
-    File file = new File(args[1]);
+    GroupDataGenerator generator = new GroupDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    } catch (ParameterException ex) {
+      jCommander.usage();
+      return;
+    }
 
-    List<GroupData> groups = generateGroups(count);
-    save(groups, file);
-
+    generator.run();
   }
 
-  private static void save(List<GroupData> groups, File file) throws IOException {
+  private void run() throws IOException {
+    List<GroupData> groups = generateGroups(count);
+    save(groups, new File (file));
+  }
+
+  private void save(List<GroupData> groups, File file) throws IOException {
 
     Writer writer = new FileWriter(file);
     for (GroupData group : groups) {
@@ -31,7 +50,7 @@ public class GroupDataGenerator {
     writer.close();
   }
 
-  private static List<GroupData> generateGroups(int count) {
+  private List<GroupData> generateGroups(int count) {
   List<GroupData> groups = new ArrayList<GroupData>();
   for (int i =0; i < count; i++) {
     groups.add(new GroupData().withName(String.format("test %s", i))
