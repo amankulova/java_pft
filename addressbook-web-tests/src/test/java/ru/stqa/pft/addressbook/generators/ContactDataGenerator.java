9cf4308bedb9a88297dd2e885301ae5e7b4e87b4
amankulova <aman.gulzhan@mail.ru>
2017-07-14 09:53:55 +0600

    Генераторя для создания контактов и групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -1,7 +1,45 @@
 package ru.stqa.pft.addressbook.generators;
 
+import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by User on 13.07.2017.
  */
 public class ContactDataGenerator {
+
+  public static void main(String[] args) throws IOException {
+    int count = Integer.parseInt(args[0]);
+    File file = new File(args[1]);
+
+    List<ContactData> contacts = generateContacts(count);
+    save(contacts, file);
+
+  }
+
+  private static void save(List<ContactData> contacts, File file) throws IOException {
+
+    Writer writer = new FileWriter(file);
+    for (ContactData contact : contacts) {
+      writer.write(String.format("%s;%s\n", contact.getFirstname(), contact.getLastname()));
+    }
+    writer.close();
+  }
+
+  private static List<ContactData> generateContacts(int count) {
+    List<ContactData> contacts = new ArrayList<ContactData>();
+    for (int i =0; i < count; i++) {
+      contacts.add(new ContactData().withFirstname(String.format("Имя %s", i))
+              .withLastname(String.format("Фамилия %s", i)));
+    }
+    return contacts;
+  }
+
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -34,8 +34,8 @@ public class GroupDataGenerator {
   private static List<GroupData> generateGroups(int count) {
   List<GroupData> groups = new ArrayList<GroupData>();
   for (int i =0; i < count; i++) {
-    groups.add(new GroupData().withName(String.format("test %i", i))
-    .withHeader(String.format("header %i", i)).withFooter(String.format("footer %i", i)));
+    groups.add(new GroupData().withName(String.format("test %s", i))
+    .withHeader(String.format("header %s", i)).withFooter(String.format("footer %s", i)));
     }
     return groups;
   }
