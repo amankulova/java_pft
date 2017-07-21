80a493f3e7c742c502ba7c3aae0c4b99f2c9b625
amankulova <aman.gulzhan@mail.ru>
2017-07-21 10:29:53 +0600

    ошибка111

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DbConnectionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DbConnectionTest.java
@@ -16,7 +16,7 @@ public class DbConnectionTest {
 
     Connection conn = null;
     try {
-      conn =  DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=");
+      conn =  DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=&serverTimezone=UTC");
       Statement st = conn.createStatement();
       ResultSet rs = st.executeQuery("select group_id, group_name, group_header, group_footer from group_list");
       Groups groups = new Groups();
