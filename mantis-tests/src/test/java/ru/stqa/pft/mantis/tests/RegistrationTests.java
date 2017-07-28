a93d06e058ceafe475807535989190d32601eef0
amankulova <aman.gulzhan@mail.ru>
2017-07-28 18:58:26 +0600

    Регистрация с не откл капчей

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -1,14 +1,47 @@
 
 package ru.stqa.pft.mantis.tests;
 
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import ru.lanwen.verbalregex.VerbalExpression;
+import ru.stqa.pft.mantis.model.MailMessage;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.List;
+
+import static org.testng.Assert.*;
 
 
-public class RegistrationTests extends TestBase
-{
+public class RegistrationTests extends TestBase {
+
+  @BeforeMethod
+  public void startMailServer() {
+    app.mail().start();
+  }
+
   @Test
-  public void testRegistration() {
-    app.registration().start("user1", "user1@localhost.localdomain");
+  public void testRegistration() throws IOException, MessagingException {
+    String user = "user1";
+    String password = "password";
+    String email = "user1@localhost.localdomain";
+    app.registration().start(user, email);
+    List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+    String confirmationLink = findConfirmationLink(mailMessages, email);
+    app.registration().finish(confirmationLink, password);
+    assertTrue(app.newSession().login(user, password));
+  }
 
+  private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
+    MailMessage mailMessage = mailMessages.stream().filter((m) -> m.to.equals(email)).findFirst().get();
+    VerbalExpression regex = VerbalExpression.regex().find("http://").nonSpace().oneOrMore().build();
+    return regex.getText(mailMessage.text);
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void stopMailServer() {
+    app.mail().stop();
   }
 }
