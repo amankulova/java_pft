8c6c32d148cb8135e2e083172a00a2ec57e0d676
amankulova <aman.gulzhan@mail.ru>
2017-07-28 19:15:26 +0600

    уник имя польз не рабоч капча

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -24,9 +24,10 @@ public class RegistrationTests extends TestBase {
 
   @Test
   public void testRegistration() throws IOException, MessagingException {
-    String user = "user1";
+    long now = System.currentTimeMillis();
+    String user = String.format("user%s", now);
     String password = "password";
-    String email = "user1@localhost.localdomain";
+    String email = String.format("user%s@localhost.localdomain", now);
     app.registration().start(user, email);
     List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
     String confirmationLink = findConfirmationLink(mailMessages, email);
