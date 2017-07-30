0fd03d325085257bc21898eaa373afdd04b8692e
amankulova <aman.gulzhan@mail.ru>
2017-07-30 17:37:03 +0600

    регистрация тесте через James исправлено

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -22,7 +22,7 @@ public class RegistrationTests extends TestBase {
     long now = System.currentTimeMillis();
     String user = String.format("user%s", now);
     String password = "password";
-    String email = String.format("user%s@localhost.localdomain", now);
+    String email = String.format("user%s@localhost", now);
     app.james().createUser(user, password);
     app.registration().start(user, email);
     // List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
